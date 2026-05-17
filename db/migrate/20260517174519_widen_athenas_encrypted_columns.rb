9828472ad3c68478330cfec6dbda1dabfa98adfd
Kennedy Rodrigues <kennedy.rodrigues1104@gmail.com>
2026-05-17 17:56:12 -0300

    fix(athenas): make encrypted column migration reversible

--- db/migrate/20260517174519_widen_athenas_encrypted_columns.rb
+++ db/migrate/20260517174519_widen_athenas_encrypted_columns.rb
@@ -1,6 +1,11 @@
 class WidenAthenasEncryptedColumns < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :ai_assistants, :encrypted_anthropic_key, :text
     change_column :ai_assistants, :encrypted_openai_key, :text
+  end
+
+  def down
+    change_column :ai_assistants, :encrypted_anthropic_key, :string
+    change_column :ai_assistants, :encrypted_openai_key, :string
   end
 end
