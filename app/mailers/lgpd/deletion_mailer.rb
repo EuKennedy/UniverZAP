7f54bea804e24c2352366724cf0589b98e59d2f6
EuKennedy <kennedy.rodrigues1104@gmail.com>
2026-05-26 12:37:27 -0300

    fix(rubocop): move Lgpd::DeletionMailer subjects to i18n locales

--- app/mailers/lgpd/deletion_mailer.rb
+++ app/mailers/lgpd/deletion_mailer.rb
@@ -2,25 +2,16 @@ class Lgpd::DeletionMailer < ApplicationMailer
   def scheduled(user)
     @user = user
     @scheduled_at = user.scheduled_for_deletion_at
-    mail(
-      to: user.email,
-      subject: '[UniverZAP] Sua exclusão de conta foi agendada'
-    )
+    mail(to: user.email, subject: I18n.t('lgpd.deletion_mailer.scheduled.subject'))
   end
 
   def cancelled(user)
     @user = user
-    mail(
-      to: user.email,
-      subject: '[UniverZAP] Exclusão de conta cancelada'
-    )
+    mail(to: user.email, subject: I18n.t('lgpd.deletion_mailer.cancelled.subject'))
   end
 
   def purged(email)
     @email = email
-    mail(
-      to: email,
-      subject: '[UniverZAP] Sua conta foi excluída permanentemente'
-    )
+    mail(to: email, subject: I18n.t('lgpd.deletion_mailer.purged.subject'))
   end
 end
